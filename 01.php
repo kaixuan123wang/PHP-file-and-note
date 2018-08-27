caf0a411cf6ff1c4f0273b4bfea491f960a1bb1e
ASUS <w995964550@163.com>
2018-08-27 14:01:02 +0800

    第一次

---
+++ 01.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>123</title>
+	<style>
+	div{
+		position: relative;
+		width: 960px;
+		height: 50px;
+		margin: 0 auto;
+	}
+		li{
+			position: relative;
+			width: 100px;
+			height: 50px;
+			float: left;
+			margin-left: 5px;
+			background: #f00;
+			text-align: center;
+			line-height: 50px;
+			list-style: none;
+		}
+		ul{
+			position: relative;
+			margin: 0 auto;
+		}
+		span{
+			clear: both;
+		}
+	</style>
+</head>
+<body>
+	<div><ul>
+		<li>x</li>
+		<li>xx</li>
+		<li>xxx</li>
+		<li>xxxx</li>
+		<li>xxxxx</li>
+		<li>xxxxxx</li>
+	</ul></div>
+	<br/>
+	<div>
+	<?php
+	for($i=01;$i<1000;$i++){
+		if($i%20== 0){
+			?>
+			<br/>
+			<?php
+				}
+			?>
+		<span><?php echo $i?></span>
+	<?php
+	}
+	?></div>
+</body>
+</html>
